1512530c85f3746cd268766e8c4b38c17b3fb003
Ioannis Alimpertis <ialimpertis@outlook.com>
2017-08-28 19:35:18 +0300

    Solution name changed encryption class added

--- Password_Protecor/Password_Protecor/Properties/Resources.Designer.cs
+++ Password_Protecor/Password_Protecor/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Hide_Your_Files_Inside_a_Picture.Properties {
+namespace Password_Protector.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Hide_Your_Files_Inside_a_Picture.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Hide_Your_Files_Inside_a_Picture.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Password_Protector.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
